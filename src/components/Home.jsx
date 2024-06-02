bc9376ddfc69bc54626e7606c5d237b33031ec1a
Varun Srivastava <VarunSrivastava19ih@gmail.com>
2024-06-02 18:58:19 +0530

    Form updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Button, Form } from "react-bootstrap";
+import { Alert, Button, Form, InputGroup } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 
@@ -10,23 +10,54 @@ export const Home = () => {
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => console.log(data);
-  console.log(watch("name"));
+  errors && console.log(errors);
+  // console.log(watch("name"));
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <Form.Group controlId="name" aria-label="Enter Name">
-        <Form.Label>Enter Name</Form.Label>
-        <Form.Control
-          placeholder="Your Name is...."
-          {...register("name")}
-          type="text"
-        />
-      </Form.Group>
-      <Form.Group controlId="email" aria-label="Enter Email">
-        <Form.Label>Enter Email</Form.Label>
-        <Form.Control type="email" {...register("email", {required: true})} placeholder="Your Email is...." />
-        {errors.email && <span>Email is a required field</span>}
-      </Form.Group>
-      <Button variant="outline-primary" type="submit">Submit</Button>
-    </Form>
+    <>
+      {errors.subject && <Alert variant="danger">
+        Enter Subject
+      </Alert>}
+      {
+        errors.paidOn && <Alert variant="danger">
+          Enter Paid On Date
+        </Alert>
+      }
+      {errors.amount && <Alert variant="danger">
+        Enter Correct Amount
+      </Alert>}
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group controlId="subject" aria-label="Enter Subject">
+          <Form.Label>Expense Subject</Form.Label>
+          <Form.Control
+            placeholder="Paid for...."
+            {...register("subject", { required: true })}
+            type="text"
+          />
+        </Form.Group>
+        <Form.Group controlId="paidOn" aria-label="Expense Paid On">
+          <Form.Label>Expense Paid On</Form.Label>
+          <Form.Control
+            type="date"
+            {...register("paidOn", { required: true })}
+            placeholder="Paid Amount On...."
+          />
+        </Form.Group>
+        <Form.Group aria-label="Expense Amount">
+          <Form.Label>Expense Amount</Form.Label>
+          <InputGroup>
+            <InputGroup.Text id="rupee-symbol">₹</InputGroup.Text>
+            <Form.Control
+              id="amount"
+              type="number"
+              step={"any"}
+              {...register("amount", { required: true, min:1 })}
+            />
+          </InputGroup>
+        </Form.Group>
+        <Button variant="outline-primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
   );
 };
